965f58ceab5efc001b4de3d04a9e8afe01fa6fa8
Artur Paikin <artur@arturpaikin.com>
2016-08-12 22:08:51 -0400

    hx+virtual-dom fix for aria- and data-

--- src/core/html.js
+++ src/core/html.js
@@ -4,5 +4,14 @@
 var vdom = require('virtual-dom')
 var hyperx = require('hyperx')
 var hx = hyperx(vdom.h)
+// fix for aria and data from https://github.com/substack/hyperx/issues/28
+// but svg is broken too
+// var hx = require('hyperx')(function (tagName, props, children) {
+//   if (!props.attributes) props.attributes = {}
+//   Object.keys(props).forEach(function (key) {
+//      if (/^aria-/.test(key) || /^data-/.test(key)) props.attributes[key] = props[key]
+//   })
+//   return vdom.h(tagName, props, children)
+// })
 
 export default hx
